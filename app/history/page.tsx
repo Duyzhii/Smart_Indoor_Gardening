515f606a59b167a719e40fbb2af7c752669b23cd
Duyzhii <duyzhii@gmail.com>
2024-08-24 11:51:50 +0700

    add_pick_date

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,23 +1,41 @@
 "use client";
 
 import React, { useState } from "react";
-import { HistoryByDayChart } from "@/components/HistoryByDayChart";
+import { PickDate, HistoryByDayChart } from "@/components/HistoryByDayChart";
 import { SelectSensors } from "@/components/SelectSensors";
+// Date Picker
+import {DateValue, parseDate, getLocalTimeZone, CalendarDate, today} from "@internationalized/date";
+
+
+function formatDate(date: DateValue): string {
+    const day = String(date.day).padStart(2, '0');
+    const month = String(date.month).padStart(2, '0');
+    const year = date.year;
+
+    return `${month}/${day}/${year}`;
+}
 
 function HistoryPage() {
     const [selectedSensor, setSelectedSensor] = useState<string>("Light sensor");
-    const [endDate, setEndDate] = useState<string>("08/24/2024");
-    const [startDate, setStartDate] = useState<string>("08/20/2024");
+    const [startDate, setStartDate] = useState<DateValue>(parseDate("2024-04-01"));
+    const [endDate, setEndDate] = useState<DateValue>(parseDate("2024-04-08"));
+
+    console.log("Start date: ", formatDate(startDate));
+    console.log("End date: ", formatDate(endDate));
 
     return (
         <div className="w-full h-full">
             <h1 className="text-3xl font-bold">History Data Chart</h1>
-            <div className = "space-y-6 w-11/12 mx-auto pt-4">
+            <div className="space-y-6 w-11/12 mx-auto pt-4">
+                <PickDate 
+                    setStartDate={setStartDate} 
+                    setEndDate={setEndDate} 
+                />
                 <div>
                     <HistoryByDayChart 
-                        sensorType= {selectedSensor}
-                        endDate = {endDate}
-                        startDate = {startDate}
+                        sensorType={selectedSensor}
+                        startDate={formatDate(startDate)}
+                        endDate={formatDate(endDate)}
                     />
                 </div>
 
--- components/HistoryByDayChart.tsx
+++ components/HistoryByDayChart.tsx
@@ -20,11 +20,10 @@ import {
 import { getSensorValueByDate } from "@/database/database";
 import { use, useEffect, useState } from "react";
 
-//Date Picker
-// import {DatePicker, DateRangePicker} from "@nextui-org/react";
-// import {DateValue, parseDate, getLocalTimeZone, CalendarDate, today} from "@internationalized/date";
-// import {useDateFormatter} from "@react-aria/i18n";
-// import { useState } from "react";
+// Date Picker
+import {DatePicker, DateRangePicker} from "@nextui-org/react";
+import {DateValue, parseDate, getLocalTimeZone, CalendarDate, today} from "@internationalized/date";
+import {useDateFormatter} from "@react-aria/i18n";
 
 interface SensorData {
     day: string;
@@ -45,122 +44,129 @@ interface HistoryByDayChartProps {
     endDate: string;
 }
 
-// export default function PickDate() {
-//     const [to, setTo] = useState<DateValue>(parseDate("2024-04-04"));
-//     const [from, setFrom] = useState<DateValue>(parseDate("2024-04-04"));
+interface PickDateProps {
+    setStartDate: (date: DateValue) => void;
+    setEndDate: (date: DateValue) => void;
+}
 
-//     let formatter = useDateFormatter({dateStyle: "full"});
+export function PickDate({ setStartDate, setEndDate }: PickDateProps) {
+    const [to, setTo] = useState<DateValue>(parseDate("2024-04-04"));
+    const [from, setFrom] = useState<DateValue>(parseDate("2024-04-04"));
   
-//     return (
-//       <div className="flex flex-row gap-4">
-//         <div className="w-full flex flex-col">
-//           <DatePicker 
-//             className="max-w-[284px] justify-center align-middle border-2 rounded-2xl" 
-//             label="From"
-//             value={from} 
-//             onChange={setFrom}
-//             defaultValue={today(getLocalTimeZone()).subtract({ days: 1 })} 
-//             maxValue={to}
-//             isInvalid = {from > to}
-//             errorMessage = "From date must be before To date"
-//             variant = "faded"
-//             isRequired
-            
-//         />
-//         </div>
-//         <DatePicker 
-//             className="max-w-[300px] border-2 rounded-2xl"
-//             label="To" 
-//             value = {to} 
-//             onChange={setTo}
-//             maxValue = {today(getLocalTimeZone())}            
-//             defaultValue={today(getLocalTimeZone()).add({ days: 1 })}
-//             isInvalid = {to < from || to > today(getLocalTimeZone())}
-//             errorMessage ={(to < from) ? "To date must be after From date" : "To date must be before today"}
-//             isRequired
-//             calendarProps={{
-//                 classNames: {
-//                     base: "bg-background border-2 rounded-2xl",
-
-//                 }
-//             }}
-//          />
-//       </div>      
-//     );
-
-// }
+    const handleFromChange = (date: DateValue) => {
+      setFrom(date);
+      setStartDate(date); 
+      console.log("Start date: ", date);
+    };
+  
+    const handleToChange = (date: DateValue) => {
+      setTo(date);
+      setEndDate(date);
+      console.log("End date: ", date);
+    };
+  
+    return (
+      <div className="flex flex-row gap-4">
+        <div className="w-full flex flex-col">
+          <DatePicker
+            className="max-w-[284px] justify-center align-middle border-2 rounded-2xl"
+            label="From"
+            value={from}
+            onChange={handleFromChange}
+            defaultValue={today(getLocalTimeZone()).subtract({ days: 1 })}
+            isInvalid={from > to}
+            errorMessage="From date must be before To date"
+            variant="faded"
+            isRequired
+          />
+        </div>
+        <DatePicker
+          className="max-w-[300px] border-2 rounded-2xl"
+          label="To"
+          value={to}
+          onChange={handleToChange}
+          maxValue={today(getLocalTimeZone())}
+          defaultValue={today(getLocalTimeZone()).add({ days: 1 })}
+          isInvalid={to < from || to > today(getLocalTimeZone())}
+          errorMessage={
+            to < from ? "To date must be after From date" : "To date must be before today"
+          }
+          isRequired
+        />
+      </div>
+    );
+  }
   
 
 export function HistoryByDayChart({ sensorType, startDate, endDate }: HistoryByDayChartProps) {
     const [sensorData, setSensorData] = useState<SensorData[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await getSensorValueByDate(sensorType, startDate, endDate);
+          setSensorData(data);
+          console.log("Sensor data: ", data);
+        } catch (err) {
+          setError(err as string);
+        } finally {
+          setLoading(false);
+        }
+      };
 
-    useEffect(() => {   
-        const fetchData = async () => {
-            try {
-                const data = await getSensorValueByDate(sensorType, startDate, endDate);
-                setSensorData(data);
-                console.log("Sensor data: ", data);
-            }
-            catch (err) {
-                setError(err as string);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
+      console.log("Sensor type: ", sensorType);
+        console.log("Start date: ", startDate);
+        console.log("End date: ", endDate);
+  
+      fetchData();
     }, [sensorType, startDate, endDate]);
-
+  
     if (loading) {
-        return <div>Loading...</div>;
+      return <div>Loading...</div>;
     }
-
+  
     if (error) {
-        return <div>Error: {error}</div>;
+      return <div>Error: {error}</div>;
     }
-
+  
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle className="mx-auto pb-5 ">
-                    {sensorType.replace(/-/g, " ")}
-                </CardTitle>
-                <CardDescription className = "w-3/5 mx-auto">
-                    {/* <PickDate /> */}
-                    {startDate} - {endDate}
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <ChartContainer config={chartConfig}>
-                    <BarChart accessibilityLayer data={sensorData}>
-                        <CartesianGrid vertical={false} />
-                        <XAxis
-                            dataKey="day"
-                            tickLine={false}
-                            tickMargin={10}
-                            axisLine={false}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                        />
-                        <ChartTooltip
-                            cursor={false}
-                            content={<ChartTooltipContent indicator="dashed" />}
-                        />
-                        <Bar
-                            dataKey="data"
-                            fill="var(--color-data)"
-                            radius={4}
-                        />
-                    </BarChart>
-                </ChartContainer>
-            </CardContent>
-            <CardFooter className="flex-col items-start gap-2 text-sm">
-                <div className="flex gap-2 font-medium leading-none">
-                    The parameters of each sensor for each day of the week{" "}
-                    <TrendingUp className="h-4 w-4" />
-                </div>
-            </CardFooter>
-        </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className="mx-auto pb-5 ">
+            {sensorType.replace(/-/g, " ")}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <BarChart accessibilityLayer data={sensorData}>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="day"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="dashed" />}
+              />
+              <Bar
+                dataKey="data"
+                fill="var(--color-data)"
+                radius={4}
+              />
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col items-start gap-2 text-sm">
+          <div className="flex gap-2 font-medium leading-none">
+            The parameters of each sensor for each day of the week{" "}
+            <TrendingUp className="h-4 w-4" />
+          </div>
+        </CardFooter>
+      </Card>
     );
-}
+  }
